9b709422769f8564a691f86fc351ec269f49e3a0
JennieKari <jennie.kari@hotmail.com>
2021-09-12 03:03:33 +0300

    Made Tuotteet/Home-views products work with sorting.

--- VillaOy/Controllers/HomeController.cs
+++ VillaOy/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -72,17 +73,71 @@ namespace VillaOy.Controllers
 
             return View();
         }
-        public ActionResult Tuotteet(string currentFilter1, string searchString1)
+        public ActionResult Tuotteet(string sortOrder, string currentFilter1, string searchString1, int? page, int? pagesize)
         {
+            ViewBag.CurrentSort = sortOrder;
+            //if-lause vb.pnsp jälkeen = Jos ensimmäinen lause on tosi ? toinen lause toteutuu : jos epätosi, niin tämä kolmas lause toteutuu
+            ViewBag.ProductNameSortParm = String.IsNullOrEmpty(sortOrder) ? "productname_desc" : "";
+            ViewBag.UnitPriceSortParm = sortOrder == "UnitPrice" ? "UnitPrice_desc" : "UnitPrice";
+
+            //Hakufiltterin laitto muistiin
+            if (searchString1 != null) //tarkistetaan onko käyttäjän antama arvo (esim. kirjain a tai sana villa) eri suuruinen kuin null
+            {
+                page = 1; //jos a-kirjainta etitään, niin vie sivulle 1 kaikki tuotteet, jossa a-kirjain
+            }
+            else
+            {
+                searchString1 = currentFilter1;
+            }
+
+            ViewBag.currentFilter1 = searchString1;
+
             var tuotteet = from p in db.Tuotteet
                            select p;
 
-            if (!String.IsNullOrEmpty(searchString1))
+            if (!String.IsNullOrEmpty(searchString1)) //Jos hakufiltteri on käytössä, niin käytetään sitä ja sen lisäksi lajitellaan tulokset
             {
-                tuotteet = tuotteet.Where(p => p.Nimi.Contains(searchString1));
+                switch (sortOrder)
+                {
+                    case "productname_desc":
+                        tuotteet = tuotteet.Where(p => p.Nimi.Contains(searchString1)).OrderByDescending(p => p.Nimi);
+                        break;
+                    case "UnitPrice":
+                        tuotteet = tuotteet.Where(p => p.Nimi.Contains(searchString1)).OrderBy(p => p.Ahinta);
+                        break;
+                    case "UnitPrice_desc":
+                        tuotteet = tuotteet.Where(p => p.Nimi.Contains(searchString1)).OrderByDescending(p => p.Ahinta);
+                        break;
+                    default:
+                        tuotteet = tuotteet.Where(p => p.Nimi.Contains(searchString1)).OrderBy(p => p.Nimi);
+                        break;
+                }
             }
+            else
+            {
+                switch (sortOrder)
+                {
+                    case "productname_desc":
+                        tuotteet = tuotteet.OrderByDescending(p => p.Nimi);
+                        break;
+                    case "UnitPrice":
+                        tuotteet = tuotteet.OrderBy(p => p.Ahinta);
+                        break;
+                    case "UnitPrice_desc":
+                        tuotteet = tuotteet.OrderByDescending(p => p.Ahinta);
+                        break;
+                    default:
+                        tuotteet = tuotteet.OrderBy(p => p.Nimi);
+                        break;
+                }
+            };
 
-            return View(tuotteet);
+            //tuotteet = tuotteet.Where(p => p.Nimi.Contains(searchString1));
+
+            int pageSize = (pagesize ?? 10); //Tämä palauttaa sivukoon taikka jos pagesize on null, niin palauttaa koon 10 riviä per sivu
+            int pageNumber = (page ?? 1); //int pageNumber on sivuparametrien arvojen asetus. Tämä palauttaa sivunumeron taikka jos page on null, niin palauttaa numeron yksi
+            return View(tuotteet.ToPagedList(pageNumber, pageSize));
+            //return View(tuotteet);
             //return View(db.Tuotteet.ToList());
         }
 
